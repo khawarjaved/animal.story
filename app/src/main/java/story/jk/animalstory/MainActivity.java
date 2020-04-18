ded148cd32dbb6af0d888b0666eb6858ed20d537
khawa <khavarjk@gmail.com>
2020-04-18 08:53:17 +0500

    listview setonitemclicklistner calling

--- app/src/main/java/story/jk/animalstory/MainActivity.java
+++ app/src/main/java/story/jk/animalstory/MainActivity.java
@@ -3,22 +3,31 @@ package story.jk.animalstory;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView list;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         String [] TitleStory = getResources().getStringArray(R.array.Title_Story);
-        String [] DetailStory = getResources().getStringArray(R.array.Detail_Story);
-        list=findViewById(R.id.list);
+        final String [] DetailStory = getResources().getStringArray(R.array.Detail_Story);
+        listView=findViewById(R.id.list);
 
         ArrayAdapter<String> adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,TitleStory);
-        list.setAdapter(adapter);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+              String t=DetailStory[position];
+            }
+        });
     }
 }
