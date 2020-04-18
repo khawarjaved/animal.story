35c641a29699ef9bc823d5971f295a86352bef89
khawa <khavarjk@gmail.com>
2020-04-18 08:43:26 +0500

    ID pass title story and detail story in main activity

--- app/src/main/java/story/jk/animalstory/MainActivity.java
+++ app/src/main/java/story/jk/animalstory/MainActivity.java
@@ -3,12 +3,21 @@ package story.jk.animalstory;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String [] TitleStory = getResources().getStringArray(R.array.Title_Story);
+        String [] DetailStory = getResources().getStringArray(R.array.Detail_Story);
+        list=findViewById(R.id.list);
+
+        ArrayAdapter
     }
 }
